66dd1176572fa733dc0bbb4fe7889ce7d2ac5110
JosepSanz1993 <bsanzgallego@gmail.com>
2025-04-03 00:13:02 +0200

    e2_5

--- Proyectos/consumo-energia-españa/src/etl.py
+++ Proyectos/consumo-energia-españa/src/etl.py
@@ -2,6 +2,9 @@ from extraction.extraction import extraction
 from datetime import date, timedelta
 from extraction.extraction import pd
 class ETL:
+    def __init__(self):
+        self.__datelist = self.__date_list()
+        self.__extract = extraction()
     def __date_list(self):
         i = date(2023,1,1)
         f = date(2023,12,31)
@@ -11,17 +14,16 @@ class ETL:
             dates.append(i.strftime("%Y-%m-%d"))
             i += delta
         return dates
-    def get_csv_temp(self):
+    def get_df_temp(self):
         d_list = self.__date_list()
         i = 0
-        extract = extraction()
         list_temp = []
         while i< len(self.date_list()):
-            if i != "2023-12-31":
-                data = extract.get_valuesAEMET(d_list[i],d_list[i+1])
+            if self.__date_list[i] != "2023-12-31":
+                data = self.__extract.get_valuesAEMET(d_list[i],d_list[i+1])
                 list_temp.append(data)
                 i+=1
         return pd.DataFrame(list_temp)
     
-    def get_csv(self,df,name):
+    def get_csv_temp(self,df,name):
         df.to_csv(f"data/raw/{name}")
