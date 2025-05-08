e84d69f3aed8b7137c43271b2ad381fd2f555309
JosepSanz1993 <bsanzgallego@gmail.com>
2025-05-08 16:27:30 +0200

    BETA_5_NO_REF

--- Proyectos/Sistema inteleligente d'analisi i prediccion d'errores /utils/cron/Cron3D.py
+++ Proyectos/Sistema inteleligente d'analisi i prediccion d'errores /utils/cron/Cron3D.py
@@ -1,31 +1,37 @@
 import threading
 import time
-from vpython import sphere,vector, color, label,rate
+from vpython import sphere, vector, color, label, rate
 
-class Cron3D(threading.Thread):
-    def init__(self, init,interval):
-        super().__init__()
+class Cron3D:
+    def __init__(self, init, interval=1.0):
         self.initial_time = init
         self.interval = interval
         self._stop = False
+        self.thread = threading.Thread(target=self._update_loop)
 
-    def run(self):
-        ball = sphere(pos=vector(self.initial_time), radius=0.5, color=color.green)
-        tag = label(pos=vector(self.initial_time,1,0),text='',height=20,box=False)
+        self.ball = sphere(pos=vector(init, 0, 0), radius=0.5, color=color.green)
+        self.tag = label(pos=vector(init, 1, 0), text='', height=20, box=False)
 
-        step = int(self.temps_inicial / self.interval)
-        for i in range(step + 1):
-            if self._deturat:
+    def start(self):
+        self.thread.start()
+
+    def _update_loop(self):
+        steps = int(self.initial_time / self.interval)
+        for i in range(steps + 1):
+            if self._stop:
                 break
-            rate(1 / self.interval)
-            temps_rest = self.temps_inicial - i * self.interval
-            ball.pos.x = temps_rest
-            tag.text = f"Remaining time: {temps_rest:.1f} s"
-            tag.pos = vector(temps_rest, 1, 0)
+            time.sleep(self.interval) 
+            remaining = self.initial_time - i * self.interval
+            self.ball.pos.x = remaining
+            self.tag.text = f"Remaining time: {remaining:.1f} s"
+            self.tag.pos = vector(remaining, 1, 0)
 
-        if not self._deturat:
-            tag.text = "Time over"
-            ball.color = color.red
+        if not self._stop:
+            self.tag.text = "Time over"
+            self.ball.color = color.red
 
-    def aturar(self):
-        self._deturat = True
+    def stop(self):
+        self._stop = True
+
+    def join(self):
+        self.thread.join()
