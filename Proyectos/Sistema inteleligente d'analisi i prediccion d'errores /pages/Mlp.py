eea3716b3dacd1941d9257efdbcd4dec01d453f0
JosepSanz1993 <bsanzgallego@gmail.com>
2025-05-22 15:05:07 +0200

    ALPHA_NEW_PROJECT

--- Proyectos/Sistema inteleligente d'analisi i prediccion d'errores /pages/Mlp.py
+++
@@ -1,9 +0,0 @@
-from utils.dash.mean_page import MeanPage
-from scripts.var_constant import *
-from bd.mongo_db import MongoDB
-mongo = MongoDB(MONGO_DB_URI, MONGO_DB_NAME)
-mongo.connect()
-data = mongo.find_data("mlp_results",{})
-mongo.close()
-InsolationPage = MeanPage()
-InsolationPage.make_eyelashes(data)
