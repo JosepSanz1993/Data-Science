d858c05c23ff07ca0e31eba19c199f8554a1c202
JosepSanz1993 <bsanzgallego@gmail.com>
2025-05-22 18:26:22 +0200

    Ramdom_forest_ok

--- Proyectos/Que-color-es?/models/random_forest.py
+++ Proyectos/Que-color-es?/models/random_forest.py
@@ -7,18 +7,29 @@ import matplotlib.pyplot as plt
 class r_forest(model_train):
     def __init__(self, data):
         self.data = self.load_data(data)
+        self.data = self.data.to_pandas()
 
     def train_model(self,colum):
-        X = self.data.drop(colum)
+        X = self.data.drop(columns=[colum])
         y = self.data[colum]
         y_encoded = y.astype("category").cat.codes
         category_mapping = dict(enumerate(y.astype("category").cat.categories))
+
         X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
         clf = RandomForestClassifier(random_state=42)
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
-        cm = confusion_matrix(y_test, y_pred)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=category_mapping.values())
+
+        all_classes = list(category_mapping.keys())
+        cm = confusion_matrix(y_test, y_pred, labels=all_classes)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[category_mapping[i] for i in all_classes])
+        
+        plt.figure(figsize=(10,8))
         disp.plot(cmap=plt.cm.Blues)
+        plt.xticks(rotation=45, ha='right') 
         plt.title("Matriz de Confusión - Random Forest")
+        plt.tight_layout()
         plt.show()
+
+r = r_forest("data/processed/color_sensor_data_processed.json")
+r.train_model("Color")
